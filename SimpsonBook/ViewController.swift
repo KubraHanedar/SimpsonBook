a44f123d967c07d086a05b7af404e170b915cf0b
Kübra Hanedar <the.hanedar@gmail.com>
2022-11-26 21:27:34 +0300

    model yazıldı

--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -16,6 +16,57 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        //Simpson Objects
+        //let homer = Simpson()
+        //homer.name = "Homer"
+        //homer.job = "Nuclear Safety"
+        //homer.image = UIImage(named: "Homer")!
+        //
+        //let marge = Simpson()
+        //marge.name = "Marge"
+        //marge.job = "Housewife"
+        //marge.image = UIImage(named: "Marge")!
+        //
+        //let lisa = Simpson()
+        //lisa.name = "Lisa"
+        //lisa.job = "Student"
+        //lisa.image = UIImage(named: "Lisa")!
+        //
+        //let bart = Simpson()
+        //bart.name = "Bart"
+        //bart.job = "Student"
+        //bart.image = UIImage(named: "Bart")!
+        //
+        //let maggie = Simpson()
+        //maggie.name = "Maggie"
+        //maggie.job = "Baby"
+        //maggie.image = UIImage(named: "Maggie")!
+        
+        
+        
+        let homer = Simpson(simpsonName: "Homer", simpsonJob: "Nuclear Safety", simpsonImage: UIImage(named: "Homer")!)
+        let marge = Simpson(simpsonName: "Marge", simpsonJob: "Housewife", simpsonImage: UIImage(named: "Marge")!)
+        let lisa = Simpson(simpsonName: "Lisa", simpsonJob: "Student", simpsonImage: UIImage(named: "Lisa")!)
+        let bart = Simpson(simpsonName: "Bart", simpsonJob: "Student", simpsonImage: UIImage(named: "Bart")!)
+        let maggie = Simpson(simpsonName: "Maggie", simpsonJob: "Baby", simpsonImage: UIImage(named: "Maggie")!)
+        
+        
+        
+        let homerArray = [homer,marge,lisa,bart,maggie]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
